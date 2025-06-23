6dd4a63057d457400729feb8a7fda9cc6565312e
Denis Ogulya <110635761+DenisOgulya@users.noreply.github.com>
2025-06-23 19:33:32 -0400

    CAt_correction

--- GOIT_PYCORE_HW-04/CAts/get.cats.py
+++ GOIT_PYCORE_HW-04/CAts/get.cats.py
@@ -1,3 +1,25 @@
 from pathlib import Path
 
-path = 
+path = Path(__file__).parent/"cat_path.txt"
+
+def get_cats_info(path):
+    new_cats_info = []
+    try:
+        with open (path, "r", encoding="utf-8") as file:
+             cats = file.readlines()
+             for cat in cats:
+                formated_cat_info = cat.strip().split(",")
+                
+                cat_formated = {
+                    "id" : formated_cat_info[0], 
+                    "name" : formated_cat_info [1],
+                    "age" : formated_cat_info [2]
+                }  
+                new_cats_info.append(cat_formated)     
+        return new_cats_info       
+    except FileNotFoundError:
+        
+        return("Не вдалося знайти файл")
+    
+cat = get_cats_info(path)
+print(cat)
